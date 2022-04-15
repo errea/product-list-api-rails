6bee0436929ed3d283a4abacfcbbcdf9a9e9cfa8
errea <erreakezy@gmail.com>
2022-04-15 13:47:36 +0100

    ADD: show method for product API

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -10,14 +10,27 @@ class Api::V1::ProductsController < ApplicationController
 # If the product saves successfully, we render the json data for the product
 # if the productdoes not save successfully, we render an error object
 def create
-    products = Product.new (
+    product = Product.new (
       name: prod_prams[:name],
       brand: prod_prams[:brand],
       price: prod_prams[:price],
       description: prod_prams[:description],
     )
+    if products.save
+        render json: products, status: 200
+    else
+        render json: {error: "Error creating product"}
+    end
 end
 
+# This method looks up the product by its id, if it is found we render the json object
+# otherwise we render an error object.
   def show
+    product = Product.find_by(id: prams[:id])
+    if product 
+        render json: product, status: 200
+    else 
+        render json: {error: "Product not found"}
+    end
   end
 end
